bf7a7e7d0411bba982810f64a876bad79d5a2994
Mario Blazek <mario.b@netgen.hr>
2016-06-27 21:20:21 +0200

    Fixed CS

--- Tests/Form/FieldTypeHandler/BirthdayTest.php
+++ Tests/Form/FieldTypeHandler/BirthdayTest.php
@@ -83,7 +83,7 @@ class BirthdayTest extends \PHPUnit_Framework_TestCase
                             'name' => 'Identifier1',
                         ),
                     ),
-                )
+                ),
             )
         );
 
