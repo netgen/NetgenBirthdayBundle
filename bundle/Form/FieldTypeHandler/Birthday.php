67a426118a8d2dce58ff2fe7260c77423932f646
Edi Modrić <edi.modric@gmail.com>
2022-02-28 12:09:49 +0100

    Revert "Remove support for netgen/ez-forms-bundle"
    This reverts commit 875d8236974b8ef3cd347061bbb4ee8e04ceceb2.

---
+++ bundle/Form/FieldTypeHandler/Birthday.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\BirthdayBundle\Form\FieldTypeHandler;
+
+use DateTimeInterface;
+use Ibexa\Contracts\Core\FieldType\Value;
+use Ibexa\Contracts\Core\Repository\Values\Content\Content;
+use Ibexa\Contracts\Core\Repository\Values\ContentType\FieldDefinition;
+use Netgen\Bundle\BirthdayBundle\Core\FieldType\Birthday as BirthdayValue;
+use Netgen\Bundle\EzFormsBundle\Form\FieldTypeHandler;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+final class Birthday extends FieldTypeHandler
+{
+    public function convertFieldValueToForm(Value $value, ?FieldDefinition $fieldDefinition = null): ?DateTimeInterface
+    {
+        return $value->date;
+    }
+
+    public function convertFieldValueFromForm($data): BirthdayValue\Value
+    {
+        if (empty($data)) {
+            $data = null;
+        }
+
+        return new BirthdayValue\Value($data);
+    }
+
+    protected function buildFieldForm(
+        FormBuilderInterface $formBuilder,
+        FieldDefinition $fieldDefinition,
+        string $languageCode,
+        ?Content $content = null
+    ): void {
+        $options = $this->getDefaultFieldOptions($fieldDefinition, $languageCode, $content);
+
+        $options['input'] = 'datetime';
+        $options['widget'] = 'choice';
+        $options['constraints'][] = new Assert\Date();
+
+        $formBuilder->add(
+            $fieldDefinition->identifier,
+            BirthdayType::class,
+            $options
+        );
+    }
+}
