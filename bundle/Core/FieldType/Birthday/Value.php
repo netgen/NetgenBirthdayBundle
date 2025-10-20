6cf400189da31f520f7d0829ce97d818e28032bf
Ante Prkacin <ante.prkacin@netgen.io>
2025-10-20 10:04:42 +0200

    NGSTACK-999 add return types to some methods, const types and use constructor promotion

--- bundle/Core/FieldType/Birthday/Value.php
+++ bundle/Core/FieldType/Birthday/Value.php
@@ -12,8 +12,6 @@ use function is_string;
 
 final class Value extends BaseValue
 {
-    public ?DateTimeImmutable $date = null;
-
     /**
      * Date format to be used by {@link __toString()}.
      */
@@ -22,9 +20,9 @@ final class Value extends BaseValue
     /**
      * Construct a new Value object and initialize with $dateTime.
      *
-     * @param \DateTimeInterface|string|null $date Date as a DateTime object or string in Y-m-d format
+     * @param DateTimeImmutable|string|null $date Date as a DateTime object or string in Y-m-d format
      */
-    public function __construct($date = null)
+    public function __construct(public DateTimeImmutable|string|null $date = null)
     {
         if ($date instanceof DateTimeImmutable) {
             $this->date = $date->setTime(0, 0);
